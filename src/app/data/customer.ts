2f2bbb8feeced8d505944403c47ccc41d62f767b
Geoff Fox <fox.geoffrey@gmail.com>
2021-06-11 11:56:30 -0700

    add customer interface

---
+++ src/app/data/customer.ts
@@ -0,0 +1,22 @@
+/* Defines the customer entity */
+export interface Customer {
+  id: number;
+  companyName: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  email1: string;
+  email2: string;
+  email3: string;
+  phone1Description: string;
+  phone2Description: string;
+  phone3Description: string;
+  phone1Value: string;
+  phone2Value: string;
+  phone3Value: string;
+  notes: string;
+}
